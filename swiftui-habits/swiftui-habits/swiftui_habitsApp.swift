3b56a090222508a694c56d408ef8e9c3acd58e90
Joaquin Rodriguez <joaquin.rodriguez@razortech.com.ar>
2024-10-10 17:19:20 -0300

    Initial commit

---
+++ swiftui-habits/swiftui-habits/swiftui_habitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_habitsApp.swift
+//  swiftui-habits
+//
+//  Created by Joaquín Rodríguez on 10/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_habitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
